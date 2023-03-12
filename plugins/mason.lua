eef9cfcebe608cb63fcabf1021890e6ffbe4ea47
kfirfitousi <kfirp84@gmail.com>
2023-03-12 22:57:46 +0200

    tweak lsp and treesitter config and fix copilot

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -5,7 +5,12 @@ return {
     "williamboman/mason-lspconfig.nvim",
     -- overrides `require("mason-lspconfig").setup(...)`
     opts = {
-      -- ensure_installed = { "lua_ls" },
+      ensure_installed = {
+        "lua_ls",
+        "eslint",
+        "tsserver",
+        "rust_analyzer",
+      },
     },
   },
   -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
@@ -13,7 +18,7 @@ return {
     "jay-babu/mason-null-ls.nvim",
     -- overrides `require("mason-null-ls").setup(...)`
     opts = {
-      -- ensure_installed = { "prettier", "stylua" },
+      ensure_installed = { "prettierd", "stylua" },
     },
   },
   {
