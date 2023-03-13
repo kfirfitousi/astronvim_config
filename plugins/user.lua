8aac89eba401d06987e87957cc3fbf5a35d9c871
kfirfitousi <kfirp84@gmail.com>
2023-03-13 21:31:21 +0200

    use zbirenbaum/copilot.lua

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,9 +9,33 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+
   {
-    "github/copilot.vim",
+    "zbirenbaum/copilot.lua",
     lazy = false,
+    config = function()
+      require("copilot").setup {
+        panel = {
+          auto_refresh = false,
+          keymap = {
+            accept = "<CR>",
+            jump_prev = "[[",
+            jump_next = "]]",
+            refresh = "gr",
+            open = "<C-G>",
+          },
+        },
+        suggestion = {
+          auto_trigger = true,
+          keymap = {
+            accept = "<C-l>",
+            prev = "<C-F>",
+            next = "<C-f>",
+            dismiss = "<C-]>",
+          },
+        },
+      }
+    end,
   },
 
   {
