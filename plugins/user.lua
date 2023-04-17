fe912d490cf9e5de13e495355d7e14078924dc88
kfirfitousi <kfirfitousi@gmail.com>
2023-04-17 16:21:01 +0300

    add battery indicator to status line

--- plugins/user.lua
+++ plugins/user.lua
@@ -45,4 +45,20 @@ return {
   {
     "ThePrimeagen/vim-be-good",
   },
+
+  {
+    "justinhj/battery.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    config = function()
+      require("battery").setup {
+        update_rate_seconds = 30,    -- Number of seconds between checking battery status
+        show_plugged_icon = true,    -- If true show a cable icon alongside the battery icon when plugged in
+        show_unplugged_icon = false, -- When true show a diconnected cable icon when not plugged in
+        show_percent = true,         -- Whether or not to show the percent charge remaining in digits
+        vertical_icons = true,       -- When true icons are vertical, otherwise shows horizontal battery icon
+      }
+    end,
+  },
 }
