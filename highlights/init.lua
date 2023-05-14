cf8cb8a30b759a22169419c05f168a43d8cc9a64
kfirfitousi <kfirfitousi@gmail.com>
2023-05-14 15:54:15 +0300

    change line numbers color

--- highlights/init.lua
+++ highlights/init.lua
@@ -1,3 +1,5 @@
-return { -- this table overrides highlights in all themes
+return {
+  -- this table overrides highlights in all themes
   -- Normal = { bg = "#000000" },
+  LineNr = { fg = "#00afff" },
 }
